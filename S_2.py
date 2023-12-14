3bac20a62ae37296bae104abf0a537db1ac44595
InGyo Jeong <ruhlesy7@gachon.ac.kr>
2023-12-15 01:24:03 +0900

    source_2

---
+++ S_2.py
@@ -0,0 +1,11 @@
+def poseDetector(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+
+    assert(len(BODY_PARTS) == out.shape[1])
+
+    points = []
