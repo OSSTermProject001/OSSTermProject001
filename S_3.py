3cc6df5341f7b3cdd778bcf1d8eca81a2f43116d
Heechan Yoo <ruhlesy7@gachon.ac.kr>
2023-12-15 01:06:42 +0900

    source_3

---
+++ S_3.py
@@ -0,0 +1,8 @@
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponging body's part.
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+        points.append((int(x), int(y)) if conf > thr else None)
